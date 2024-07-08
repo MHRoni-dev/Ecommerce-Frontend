d5168caebb6541917439af37fe9e0665d8596bb0
MHRoni-dev <mdmonirhossain.github@gmail.com>
2024-07-08 14:04:00 +0600

    [init] product detail page with slug parameter

---
+++ app/product/[slug]/page.js
@@ -0,0 +1,10 @@
+import ProductDetail from '@/components/product/productDetail'
+import React from 'react'
+
+export default function ProductDetailPage() {
+  return (
+    <div>
+      <ProductDetail />
+    </div>
+  )
+}
