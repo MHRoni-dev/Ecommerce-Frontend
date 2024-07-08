849d15646a51b55c69b9ed47817c6ac046cc644a
MHRoni-dev <mdmonirhossain.github@gmail.com>
2024-07-08 13:59:28 +0600

    [test] productCard and productContainer in view

--- app/page.js
+++ app/page.js
@@ -1,10 +1,19 @@
+import ProductConatiner from "@/components/product/container";
+import ProductCard from "@/components/product/productCard";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>
-        Yoo
-      </h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-8">
+      <ProductConatiner>
+      <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </ProductConatiner>
     </main>
   );
 }
