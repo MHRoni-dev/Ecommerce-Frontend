d65aff597e92b04e3e362c6db553e06d6f8f04c6
MHRoni-dev <mdmonirhossain.github@gmail.com>
2024-07-08 14:01:26 +0600

    [init] size select for product purchase

---
+++ components/product/productDetail/SizeSelect.jsx
@@ -0,0 +1,32 @@
+"use client"
+import React, { useState } from 'react'
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+
+export default function SizeSelect() {
+    const [selected, setSelect] = useState(null)
+
+  return (
+    // <Select>
+    //         <SelectTrigger>
+    //             <SelectValue placeholder="size" />
+    //         </SelectTrigger>
+    //         <SelectContent>
+    //             <SelectGroup>
+    //                 <SelectLabel>Sizes</SelectLabel>
+    //                 <SelectItem value="m">M</SelectItem>
+    //                 <SelectItem value="l">L</SelectItem>
+    //                 <SelectItem value="xl">XL</SelectItem>
+    //                 <SelectItem value="xxl">XXL</SelectItem>
+    //             </SelectGroup>
+    //         </SelectContent>
+    //     </Select>
+    <div className=' flex items-center gap-4'>
+    <Button variant="secondary" className={cn('w-10 h-8  border rounded-sm flex items-center justify-center p-2 hover:text-primary-foreground hover:bg-primary', selected === 0 && 'bg-primary text-primary-foreground ')} onClick={()=>setSelect(0)}>M</Button>
+    <Button variant="secondary" className={cn('w-10 h-8  border rounded-sm flex items-center justify-center p-2 hover:text-primary-foreground hover:bg-primary', selected === 1 && 'bg-primary text-primary-foreground  ')} onClick={()=>setSelect(1)}>L</Button>
+    <Button variant="secondary" className={cn('w-10 h-8  border rounded-sm flex items-center justify-center p-2 hover:text-primary-foreground hover:bg-primary', selected === 2 && 'bg-primary text-primary-foreground  ')} onClick={()=>setSelect(2)}>XL</Button>
+    <Button variant="secondary" className={cn('w-10 h-8  border rounded-sm flex items-center justify-center p-2 hover:text-primary-foreground hover:bg-primary', selected === 3 && 'bg-primary text-primary-foreground  ')} onClick={()=>setSelect(3)}>XXL</Button>
+</div>
+  )
+}
