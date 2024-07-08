ca358cd91a33bedda0355c1c6b16865b73c87418
MHRoni-dev <mdmonirhossain.github@gmail.com>
2024-07-08 14:02:16 +0600

    [init] product detail for product details page

---
+++ components/product/productDetail/Detail.jsx
@@ -0,0 +1,17 @@
+import { CardDescription, CardTitle } from '@/components/ui/card'
+import React from 'react'
+
+export default function Detail() {
+  return (
+    <div className='flex justify-between'>
+        <div>
+            <CardTitle >Tshirt</CardTitle>
+            <CardDescription>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At illo officia labore?</CardDescription>
+        </div>
+        <div  className="w-40 flex flex-col items-end">
+            <CardTitle>54 <span className='text-base -ml-1'>$</span></CardTitle>
+            <CardDescription className="line-through">60$</CardDescription>
+        </div>
+    </div>
+  )
+}
