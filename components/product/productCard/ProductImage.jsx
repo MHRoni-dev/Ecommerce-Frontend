89140a481f4ad3a93c86e4acb12dcd3cf721eb8a
MHRoni-dev <mdmonirhossain.github@gmail.com>
2024-07-09 02:12:16 +0600

    [added] hover functionality in detail Image

--- components/product/productCard/ProductImage.jsx
+++ components/product/productCard/ProductImage.jsx
@@ -9,8 +9,8 @@ export default function ProductImage() {
 
   return (
     <div onMouseEnter={()=>setHover(true)} onMouseLeave={()=>setHover(false)}>
-        <Image src="/tshirt.avif" fill  className='bg-cover'   alt='Product Image' >
-            </Image>
+        <Image src="/tshirt.avif" fill  className='bg-cover'   alt='Product Image' />
+          
             <div className={cn('w-full h-full bg-slate-900/50 top-0 left-0 right-0',hover ? "absolute" : "hidden")}>
             </div>
     </div>
--- components/product/productDetail/Images.jsx
+++ components/product/productDetail/Images.jsx
@@ -5,10 +5,43 @@ import React, {useState} from 'react'
 
 export default function ProductDetailImage() {
     const [src, setSrc] = useState("/tshirt.avif")
+    const [transform, setTransform] = useState("")
+    
+
+    const handleMouseMove = (e) =>{
+      const { offsetX, offsetY, target } = e.nativeEvent;
+      const { offsetWidth, offsetHeight } = target;
+
+      // Calculate the center of the element
+      const centerX = offsetWidth / 2;
+      const centerY = offsetHeight / 2;
+
+      // Calculate the position of the mouse relative to the center of the element
+      const relativeX = offsetX - centerX;
+      const relativeY = offsetY - centerY;
+
+      // Normalize these values to be between -1 and 1
+      const normalizedX = (relativeX / centerX);
+      const normalizedY = (relativeY / centerY);
+
+      // Translate values in percentage (adjust these factors to control the strength of translation)
+      const translateX = normalizedX * 16.67; // 100 / 6 = 16.67
+      const translateY = normalizedY * 16.67; // 100 / 6 = 16.67
+
+      setTransform(`translate(${-translateX}%, ${-translateY}%) scale(1.4)`);
+    }
+
+    
+
+    const handleMouseLeave = () => {
+      setTransform('')
+    }
+
+
   return (
     <div >
-        <div className='w-full max-w-[400px]  max-h-[400px] relative h-[50vh] border'>
-            <Image src={src} fill sizes='100vw' alt="Product Imge" />
+        <div className='w-full max-w-[400px]  max-h-[400px] relative h-[50vh] border overflow-hidden' onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave}>
+            <Image src={src} fill sizes='100vw' alt="Product Imge" style={{transform }}  />
         </div>
         <div className='relative flex gap-8 mt-6 overflow-auto w-full pb-2'>
             <Image src="/tshirt.avif" width={100} height={80} alt="Product Imge" className={cn('border rounded-sm', src === "/tshirt.avif" && 'border-primary')} onClick={()=>setSrc('/tshirt.avif')} />
