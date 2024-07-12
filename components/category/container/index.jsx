2cfddeb053457ff27baf26e49889a065eb5dda37
MHRoni-dev <mdmonirhossain.github@gmail.com>
2024-07-12 10:10:35 +0600

    [update] category container is now swapable

--- components/category/container/index.jsx
+++ components/category/container/index.jsx
@@ -1,7 +1,20 @@
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
 import React from 'react'
 
 export default function CategoryContainer({children}) {
+  
   return (
-    <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7  gap-6 flex-wrap my-4'>{children}</div>
+    // <div className='flex gap-x-10  gap-6 my-4 overflow-hidden'>{children}</div>
+    <Carousel  >
+        <div className='overflow-hidden rounded relative my-4'>
+            <CarouselContent  className=''>
+                {children.map((item,i) => (
+                  <CarouselItem key={i} className="basis-0 min-w-fit px-4">
+                    {item}
+                </CarouselItem>
+                ))}
+            </CarouselContent>
+        </div>
+    </Carousel>
   )
 }
