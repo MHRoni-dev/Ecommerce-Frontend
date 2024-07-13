883935bc3e31cf15392fd9d82d8b8507b416447e
MHRoni-dev <mdmonirhossain.github@gmail.com>
2024-07-13 17:10:04 +0600

    [mod] product card height width changed

--- components/product/productCard/index.jsx
+++ components/product/productCard/index.jsx
@@ -6,33 +6,32 @@ import { Button } from '@/components/ui/button'
 
 export default function ProductCard() {
   return (
-    <Card className="min-w-[280px] w-full max-w-[340px] overflow-hidden">
-        <CardHeader className="relative h-60 group">
+    <Card className="min-w-[150px] w-full max-w-[340px] overflow-hidden">
+        <CardHeader className="relative h-40 group">
             <ProductImage />
         </CardHeader>
-        <CardContent>
-            <div className='flex justify-between mt-4'>
+        <CardContent className="px-2">
+            <div className='flex justify-between mt-4 flex-col sm:flex-row'>
                 <div>
                     <CardTitle>Tshirt </CardTitle>
                     <CardDescription>
-                        <span className='text-black'><span className='text-lg'>500$</span></span>
+                        <span className='text-black'><span className='text-lg'>500$</span><span className='text-muted-foreground line-through'>560$</span></span>
                     </CardDescription>
                 </div>
                 <div>
-                    <div className='flex justify-end'>
-                        <Star className='w-5  text-primary' fill/>
-                        <Star className='w-5' fill/>
-                        <Star className='w-5'fill/>
-                        <Star className='w-5'fill/>
-                        <Star className='w-5'/>
+                    <div className='flex sm:justify-end'>
+                        <Star className='w-[18px]  text-transparent' fill="hsl(20 70% 50%)"/>
+                        <Star className='w-[18px]  text-transparent' fill="hsl(20 70% 50%)"/>
+                        <Star className='w-[18px]  text-transparent' fill="hsl(20 70% 50%)"/>
+                        <Star className='w-[18px]  text-transparent' fill="hsl(20 70% 50%)"/>
+                        <Star className='w-[16px] text-gray-400'/>
                     </div>
-                    <CardDescription className="text-end">4.0/5.0</CardDescription>
+                    <CardDescription className="hidden sm:text-end sm:block">4.0/5.0</CardDescription>
                 </div>
             </div>
         </CardContent>
-        <CardFooter className='-mt-2 flex justify-between items-center'>
-            <Button>Add to Cart</Button>
-            <Button variant="outline">Checkout</Button>
+        <CardFooter className='-mt-2 flex justify-between items-center px-2'>
+            <Button  size="sm" className="w-full">Add to Cart</Button>
         </CardFooter>
     </Card>
   )
