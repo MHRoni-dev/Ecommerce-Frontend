278c2b333d2a57b2dbe0aa2bdad80c9f29cf7027
MHRoni-dev <mdmonirhossain.github@gmail.com>
2024-07-13 15:37:17 +0600

    [added] filter title component created

--- components/ui/title.jsx
+++ components/ui/title.jsx
@@ -6,3 +6,10 @@ export function SectionTitle({children}) {
     <CardTitle className="border-l-8 py-2 pl-2 mt-16  border-highlight w-fit">{children}</CardTitle>
   )
 }
+
+export function FilterTitle({children}) {
+
+  return (
+    <CardTitle className="text-lg border-b mt-2 pb-1 mb-2 ">{children}</CardTitle>
+  )
+}
