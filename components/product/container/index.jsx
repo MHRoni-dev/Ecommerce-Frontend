81eda9ce52cbc9143db2e69f4f31e19a6fe918e2
MHRoni-dev <mdmonirhossain.github@gmail.com>
2024-07-14 09:37:37 +0600

    [added] added homepage variant for product container

--- components/product/container/index.jsx
+++ components/product/container/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
+export default function ProductConatiner({variant,children}) {
 
-export default function ProductConatiner({children}) {
+  if(variant === "homePage"){
+    return (
+      <div className="flex justify-center">
+        <div className="grid grid-cols-2 sm:grid-cols-3  md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 w-full flex-1 my-4">
+          {children}
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="flex justify-center">
-      <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 w-full flex-1">
+      <div className="grid grid-cols-2 sm:grid-cols-3  md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-8 w-full flex-1 my-4">
         {children}
       </div>
       </div>
