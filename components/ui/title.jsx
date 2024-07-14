eed385559b06bb4306c822e5e1f7301a67dd755e
MHRoni-dev <mdmonirhossain.github@gmail.com>
2024-07-14 09:31:37 +0600

    [mod] added className support in Title component

--- components/ui/title.jsx
+++ components/ui/title.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { CardTitle } from './card'
+import { cn } from '@/lib/utils'
 
-export function SectionTitle({children}) {
+export function SectionTitle({className,children}) {
   return (
-    <CardTitle className="border-l-8 py-2 pl-2 mt-16  border-highlight w-fit">{children}</CardTitle>
+    <CardTitle className={cn("border-l-8 py-2 pl-2 mt-16  border-highlight w-fit",className)}>{children}</CardTitle>
   )
 }
 
-export function FilterTitle({children}) {
+export function FilterTitle({className, children}) {
 
   return (
-    <CardTitle className="text-lg border-b mt-2 pb-1 mb-2 ">{children}</CardTitle>
+    <CardTitle className={cn("text-lg border-b mt-2 pb-1 mb-2 ", className)}>{children}</CardTitle>
   )
 }
