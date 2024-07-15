97ad2a45470a96624533f0e9788140d072af6546
MHRoni-dev <mdmonirhossain.github@gmail.com>
2024-07-15 12:08:23 +0600

    [init] floating button container for layouting floating button

---
+++ components/floatingButton/FloatingButtonContainer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FloatingButtonContainer({children}) {
+  return (
+    <div className='fixed bottom-6 right-8 flex flex-col gap-2'>{children}</div>
+  )
+}
