d68b5fd430b6e6a63d4c4b284dd0b5a84532574c
MHRoni-dev <mdmonirhossain.github@gmail.com>
2024-07-19 16:15:21 +0600

    [fix] fixed z-index of the floation button container

--- components/floatingButton/FloatingButtonContainer.jsx
+++ components/floatingButton/FloatingButtonContainer.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function FloatingButtonContainer({children}) {
   return (
-    <div className='fixed bottom-6 right-8 flex flex-col gap-2'>{children}</div>
+    <div className='fixed bottom-6 right-8 flex flex-col gap-2 z-50'>{children}</div>
   )
 }
