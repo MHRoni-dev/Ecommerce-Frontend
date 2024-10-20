e7f4fdcb7702f184daf4c6cc2331449be60cb193
MHRoni-dev <mdmonirhossain.github@gmail.com>
2024-10-20 16:47:35 +0600

    refactor(featureCard): add 2 circle design in fearure banner

--- components/product/featureProduct/Banner.jsx
+++ components/product/featureProduct/Banner.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 export default function     FeatureBanner() {
   return (
-        <div className='border border-highlight w-full min-h-14 flex justify-between items-center px-4'>
+        <div className='border border-highlight/20 drop-shadow w-full min-h-14 flex justify-between items-center px-8 rounded-2xl bg-highlight/10'>
             <span className='text-highlight text-[15px] md:text-base font-semibold tracking-wide'>4000+ sell</span>
+            <div className='bg-highlight/70 rounded-full text-white h-3 w-3' />
             <span className='text-highlight text-[15px] md:text-base font-semibold tracking-wide'>200+ review</span>
+            <div className='bg-highlight/70 rounded-full text-white h-3 w-3' />
             <span className='text-highlight text-[15px] md:text-base font-semibold tracking-wide'>4.5 star</span>
         </div>
   )
