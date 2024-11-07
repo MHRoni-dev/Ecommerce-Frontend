c1d1fefcb2e59c52c6a1e81242164f01c29de0ae
MHRoni-dev <mdmonirhossain.github@gmail.com>
2024-11-07 23:13:59 +0600

    feat(redux),feat(shadcn): add and setup redux persist and add avater and popover

--- store/index.js
+++ store/index.js
@@ -1,15 +1,31 @@
 import { api } from './api';
 
-const { configureStore } = require('@reduxjs/toolkit');
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+
+const { configureStore, combineReducers } = require('@reduxjs/toolkit');
 const { setupListeners } = require('@reduxjs/toolkit/query');
 
+
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const rootReducer = combineReducers({
+  [api.reducerPath]: api.reducer,
+
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
 const store = configureStore({
-  reducer: {
-    [api.reducerPath]: api.reducer
-  },
+  reducer: persistedReducer,
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(api.middleware),
 })
 
 setupListeners(store.dispatch)
 
+export const persistor = persistStore(store)
 export default store
